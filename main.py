9bf0677de69d5e2b98bffc0bccae62d47b63f8cf
Michael Yudin <mihelitto@gmail.com>
2021-11-14 15:33:33 +0300

    Add cli args

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import subprocess
 import time
+import argparse
 import psutil
 
 
 def main():
-    proc = subprocess.Popen(['python', 'test_process.py'])
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument(
+        'program',
+        type=str,
+        help='path to the program',
+    )
+    args_parser.add_argument(
+        '-i', '--interval',
+        type=float,
+        help='statistics collection interval',
+        default=0.5,
+    )
+
+    args = args_parser.parse_args()
+    interval = args.interval
+    program = args.program.split()
+
+    proc = subprocess.Popen(program)
     python_process = psutil.Process(proc.pid)
+
     while psutil.pid_exists(proc.pid):
         cpu_load = python_process.cpu_percent()
         working_set = python_process.memory_info().rss
@@ -15,7 +34,7 @@ def main():
         print('Working set memory: ', working_set)
         print('Private memory: ', private_bytes)
         print('Open handles: ', file_handles)
-        time.sleep(0.1)
+        time.sleep(interval)
 
 
 if __name__ == '__main__':
