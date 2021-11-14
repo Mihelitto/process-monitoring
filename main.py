3e2fb651193a039b560239522d749b57e584bc1d
Michael Yudin <mihelitto@gmail.com>
2021-11-14 16:15:03 +0300

    Add logging to file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 import argparse
+import csv
 import psutil
 
 
@@ -17,24 +18,42 @@ def main():
         help='statistics collection interval',
         default=0.5,
     )
+    args_parser.add_argument(
+        '-l', '--log',
+        type=str,
+        help='path to save log file',
+        default='log.csv',
+    )
 
     args = args_parser.parse_args()
     interval = args.interval
     program = args.program.split()
+    log_path = args.log
 
     proc = subprocess.Popen(program)
     python_process = psutil.Process(proc.pid)
 
-    while psutil.pid_exists(proc.pid):
-        cpu_load = python_process.cpu_percent()
-        working_set = python_process.memory_info().rss
-        private_bytes = python_process.memory_info().private
-        file_handles = len(python_process.open_files())
-        print('CPU load: ', cpu_load)
-        print('Working set memory: ', working_set)
-        print('Private memory: ', private_bytes)
-        print('Open handles: ', file_handles)
-        time.sleep(interval)
+    with open(log_path, 'w', newline='') as log_file:
+        log_writer = csv.writer(log_file, dialect='excel', delimiter=',')
+        log_writer.writerow([
+            'CPU load',
+            'Working set memory',
+            'Private memory',
+            'Open handles',
+        ])
+        while psutil.pid_exists(proc.pid):
+            cpu_load = python_process.cpu_percent()
+            working_set = python_process.memory_info().rss
+            private_bytes = python_process.memory_info().private
+            file_handles = len(python_process.open_files())
+            log_writer.writerow([
+                cpu_load,
+                working_set,
+                private_bytes,
+                file_handles,
+            ])
+
+            time.sleep(interval)
 
 
 if __name__ == '__main__':
